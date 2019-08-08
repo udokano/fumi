c0bd3c929d06ab25ef6481d034f4002d50236402
伊藤京子 <okano@u-d.jp>
2019-08-08 10:05:12 +0900

    形成外科ページ作成

---
+++ page-keiseigeka.php
@@ -0,0 +1,103 @@
+<?php/*
+Template Name: 形成外科治療 
+*/
+
+?>
+<?php get_header()?>
+  
+  <div class="page-head">
+
+           <h1 class="tc f-ryu">施術：形成外科治療</h1>  
+  
+            <?php echo breadcrumb_func(); ?>
+
+
+    </div>
+ 
+  
+ <section class="ope-intro bg-cp">
+     
+     
+     
+     <div class="inner flex">
+         
+         
+         
+                 
+                 <div class="text box-size">
+                 
+                                  <h2>形成外科治療</h2>
+                     
+
+                     <p class="ls1 gosic">
+                         形成外科は、手術による治療を必要とする外表上の異常をできる限り外見に気を配りつつ治療する外科です。
+                     </p>
+                     
+                     
+                 
+                 
+                 
+         
+         
+             </div>
+     
+     </div>
+     
+     
+
+</section> 
+  
+
+<section class="doctor-carrer">
+    
+    
+    <div class="inner gosic flex">
+        
+     
+        <div class="left box-size">
+        
+            <h3 class="cw">経歴</h3>
+            
+            <ul>
+                <li><span>2003年</span>帝京大学医学部　卒業</li>
+                <li><span>2003年</span>帝京大学病院形成外科　入局</li>
+                <li><span>2011年</span>市立旭川病院皮膚科　入局</li>
+                <li><span>2015年</span>トキコクリニック　淀屋橋院長　就任</li>
+                <li><span>2016年</span>トキコクリニック　梅田院長　就任</li>
+                <li><span>2019年</span>fumi beauty clinic　院長　就任</li>
+                
+            </ul>
+            
+        
+        
+        </div>
+        
+        
+        <div class="right box-size">
+        
+            <h3 class="cw">所属</h3>
+            
+            <ul>
+                <li>帝京大学医学部　卒業</li>
+                <li>帝京大学病院形成外科　入局</li>
+                <li>市立旭川病院皮膚科　入局</li>
+                <li>トキコクリニック　淀屋橋院長　就任</li>
+                <li>トキコクリニック　梅田院長　就任</li>
+                <li>fumi beauty clinic　院長　就任</li>
+                
+            </ul>
+            
+        
+        
+        </div>
+      
+    
+    </div>
+    
+    
+   
+</section>
+
+ 
+  
+  <?php get_footer();?>
