fa06c92b79ed84e84f3f1ef9c1627d4c1d494221
伊藤京子 <okano@u-d.jp>
2019-08-08 20:13:35 +0900

    鼻の糸症例まで完了

--- page-nose.php
+++ page-nose.php
@@ -9,124 +9,238 @@ Template Name: 鼻の糸
   <?php echo breadcrumb_func(); ?> </div>
 <section class="ope-intro bg-cp">
   <div class="inner flex al-cent">
-      
-       <div class="thumb-sq">
-      
-          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/tenteki/thumb01.jpg" alt="サムネイル">
-      
-      </div>
-      
-      
+    <div class="thumb-sq"> <img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/thumb01.jpg" alt="サムネイル"> </div>
     <div class="text box-size">
-      <h2>美容点滴</h2>
-      <p class="ls1 gosic"> 血管を通してダイレクトに点滴の成分を投与し、体の内面から綺麗にすることを目的にする施術です。しわ・くすみの解消や疲労回復など幅広い効果が期待できます。
-      </p>
+      <h2>鼻の糸（隆鼻術）</h2>
+      <p class="ls1 gosic">溶ける医療用糸を使用する、切らない鼻の形成施術です。
+        収糸を使用するので、体内に異物が残らず安全性が高い隆鼻術です。
+        シャープな鼻にしたり、鼻筋をハッキリさせることが可能です。 </p>
     </div>
-      
-      
-     
-      
   </div>
 </section>
-<section class="features layout2">
+<section class="features layout1">
   <div class="inner-box2">
-      
-      <h2 class="cp gosic tc ls1">美容点滴の特徴</h2>
-      
-      
-      
-      <ul class="flex">
-          <li class="box-size">
-              <div class="points f-ryu">Point.1</div>
-          <h3 class="gd tc pc">体の内側から健康になれる</h3> <h3 class="gd tc sp">体の内側から<br>健康になれる</h3>
-              
-              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/tenteki/thumb02.jpg" alt="サムネイル"></div>
-              
-              <p class="gosic">表面上の美しさだけではなく、体の内側からリフレッシュをして、内側からの自然な美しさが得られます。</p>
-          
-          </li>
-          
-          <li class="box-size">
-              <div class="points f-ryu">Point.2</div>
-          <h3 class="gd tc pc">短時間で効果を実感できる</h3><h3 class="gd tc sp">短時間で効果<br>を実感できる</h3>
-              
-              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/tenteki/thumb03.jpg" alt="サムネイル"></div>
-          
-              <p class="gosic">他の施術と比較して、短期間で効果を出すことが可能です。</p>
-              
-          </li>
-          
-          <li class="box-size">
-              <div class="points f-ryu">Point.3</div>
-          <h3 class="gd tc pc">不足している栄養素の摂取</h3><h3 class="gd tc sp">不足している<br>栄養素の摂取</h3>
-              
-              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/tenteki/thumb04.jpg" alt="サムネイル"></div>
-          
-              <p class="gosic">ビタミン類の不足しがちな、栄養素を取り入れて、「ニキビができる」などの悩みを取り除きます。</p>
-              
-              
-          </li>
-          
-          <li class="box-size">
-              <div class="points f-ryu">Point.4</div>
-          <h3 class="gd tc">疲労回復</h3>
-              
-              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/tenteki/thumb05.jpg" alt="サムネイル"></div>
-              
-              <p class="gosic">疲労回復に必要な成分を取り入れて、「寝ても疲れが取れない」といった悩みを解消します。</p>
-          
-          </li>
-      </ul>
-      
-    
+    <h2 class="cp gosic tc ls1">鼻の糸の特徴</h2>
+    <div class="box">
+      <div class="desc box-size bg-grade">
+        <div class="points f-ryu cw">Point.1</div>
+        <h3 class="gd ls1">鼻先を<br>
+          切らずに整える</h3>
+        <p class="gosic">メスを使わずに、鼻先を整えることが可能なので、お手軽に受けられる整形施術です。</p>
+      </div>
+      <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/point01.jpg" alt="ポイントエリアサムネイル"></div>
+    </div>
+    <div class="box cf">
+      <div class="desc box-size bg-grade">
+        <div class="points f-ryu cw">Point.2</div>
+        <h3 class="gd ls1">短時間の施術</h3>
+        <p class="gosic">施術時間が約15分〜30分なので、お忙しい方でも、スキマ時間で施術を受けること可能です。</p>
+      </div>
+      <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/point02.jpg" alt="ポイントエリアサムネイル"></div>
+    </div>
+    <div class="box">
+      <div class="desc box-size bg-grade">
+        <div class="points f-ryu cw">Point.3</div>
+        <h3 class="gd ls1">団子鼻解消</h3>
+        <p class="gosic">気になる団子鼻を解消して、シャープで美しい鼻先に近づけられます。</p>
+      </div>
+      <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/point03.jpg" alt="ポイントエリアサムネイル"></div>
+    </div>
+    <div class="box cf">
+      <div class="desc box-size bg-grade">
+        <div class="points f-ryu cw">Point.4</div>
+        <h3 class="gd ls1">ダウンタイムが<br>
+          ほとんどない</h3>
+        <p class="gosic">メスを使わないため、腫れやリスクがほとんどなく、すぐに普段の日常生活に戻れます。</p>
+      </div>
+      <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/point04.jpg" alt="ポイントエリアサムネイル"></div>
+    </div>
   </div>
 </section>
-
-
-<section class="price">
-
-
-    <div class="inner-box2">
+<section class="ope-kind">
+<div class="wrap bg-cg2">
+  <div class="inner">
+    <div class="box flex">
+      <div class="desc">
+        <h3 class="ls1">Gメッシュ</h3>
+        <p class="gosic">PCL(ポリカプロラクトン)素材の特殊なメッシュ状の吸収糸を特殊な針で挿入し、鼻筋にボリュームを出す施術です。腫れや痛みが少なく、鼻筋に自然なボリュームを与えます。</p>
+      </div>
+      <div class="thumb"> <img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/thumb02.jpg" alt="サムネイル"> </div>
+    </div>
+  </div>
+</div>
+<div class="wrap">
+<div class="inner">
+  <div class="box flex">
+    <div class="thumb"> <img src="<?php echo get_template_directory_uri(); ?>/img/operation/nose/thumb03.jpg" alt="サムネイル"> </div>
+    <div class="desc">
+      <h3 class="ls1">G-COG</h3>
+      <p class="gosic mb20">PCL(ポリカプロラクトン)素材を使用したスレッドを鼻に挿入することで、鼻の形を整えたり、鼻筋を通したりする施術に適したスレッド療法です。</p>
+      <p class="gosic">柔軟でリフティング力・ボリューム力、長期持続性を高めた革新的形状のスレッドで鼻筋の通ったシャープな鼻に整えます。</p>
+    </div>
+  </div>
+</div>
+</section>
     
+    
+<section class="case">
+  
+    <div class="inner">
+    
+         <h2 class="tc">症例</h2>
         
-        <h2 class="tc ls1">料金表</h2>
+        <div class="case-wrap flex">
+        
+              <div class="case-box flex">
+                  
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/befor.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術前</p>
+                  
+                  </div>
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/after.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術◯回後</p>
+                  
+                  </div>
         
         
-        <div class="price-list gosic">
+              </div>
+            
+              <div class="case-box flex">
+                  
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/befor.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術前</p>
+                  
+                  </div>
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/after.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術◯回後</p>
+                  
+                  </div>
         
-            <dl class="flex">
-                <dt>内容</dt>
-                <dd class="f-bold">税別</dd>
-            </dl>
+        
+              </div>
             
-             <dl class="flex">
-                <dt>内容入ります</dt>
-                <dd>¥00,000</dd>
-            </dl>
+              <div class="case-box flex">
+                  
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/befor.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術前</p>
+                  
+                  </div>
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/after.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術◯回後</p>
+                  
+                  </div>
+        
+        
+              </div>
             
-             <dl class="flex">
-                <dt>内容入ります</dt>
-                <dd>¥00,000</dd>
-            </dl>
-            
-            
-             <dl class="flex">
-                <dt>内容入ります</dt>
-                <dd>¥00,000</dd>
-            </dl>
-            
-             <dl class="flex">
-                <dt>内容入ります</dt>
-                <dd>¥00,000</dd>
-            </dl>
+              <div class="case-box flex">
+                  
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/befor.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術前</p>
+                  
+                  </div>
+                  
+                  <div class="box">
+                      <div class="thumb">
+                          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/common/after.jpg" alt="">
+                      
+                      </div>
+                      
+                      <p class="gosic tc">施術◯回後</p>
+                  
+                  </div>
+        
+        
+              </div>
         
         </div>
         
         
+        
+        <div class="note box-size gosic">
+        
+            <p>リスク副作用</p>
+            <p>・体質により稀にメイクで隠せる程度の内出血が出る場合があります。</p>        
+        </div>
+    
     
     
     </div>
-
-
+    
+    
+    
+    
+    
+    
+</section>    
+    
+<section class="price">
+  <div class="inner-box2">
+    <h2 class="tc ls1">料金表</h2>
+    <div class="price-list gosic">
+      <dl class="flex">
+        <dt>内容</dt>
+        <dd class="f-bold">税別</dd>
+      </dl>
+      <dl class="flex">
+        <dt>内容入ります</dt>
+        <dd>¥00,000</dd>
+      </dl>
+      <dl class="flex">
+        <dt>内容入ります</dt>
+        <dd>¥00,000</dd>
+      </dl>
+      <dl class="flex">
+        <dt>内容入ります</dt>
+        <dd>¥00,000</dd>
+      </dl>
+      <dl class="flex">
+        <dt>内容入ります</dt>
+        <dd>¥00,000</dd>
+      </dl>
+    </div>
+  </div>
 </section>
 <?php get_footer();?>
