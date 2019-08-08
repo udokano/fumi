bc597bfd9c018e9be983d65d72eda76bc0746a6a
伊藤京子 <okano@u-d.jp>
2019-08-08 14:59:00 +0900

    メディカルエステページ作成コーディング済み

---
+++ page-medikaru.php
@@ -0,0 +1,136 @@
+<?php/*
+Template Name: メディカルエステ 
+*/
+
+?>
+<?php get_header()?>
+<div class="page-head">
+  <h1 class="tc f-ryu">施術：メディカルエステ</h1>
+  <?php echo breadcrumb_func(); ?> </div>
+<section class="ope-intro bg-cp">
+  <div class="inner flex al-cent">
+      
+       <div class="thumb-sq">
+      
+          <img src="<?php echo get_template_directory_uri(); ?>/img/operation/medikaru/thumb01.jpg" alt="サムネイル">
+      
+      </div>
+      
+      
+    <div class="text box-size">
+      <h2>美肌治療(メディカルエステ)</h2>
+      <p class="ls1 gosic mb20">光治療や高周波治療、ピーリング治療やイオン導入など、肌に過度な負担がかからない美容医療のことです。 </p>
+        
+  <p class="ls1 gosic">
+美顔器やエステを受けるような感覚なので、、メスをいれたりダウンタイムが伴うような施術はございません。
+
+      </p>
+    </div>
+      
+      
+     
+      
+  </div>
+</section>
+<section class="features layout2">
+  <div class="inner-box2">
+      
+      <h2 class="cp gosic tc ls1">美肌治療(メディカルエステ)の特徴</h2>
+      
+      
+      
+      <ul class="flex">
+          <li class="box-size">
+              <div class="points f-ryu">Point.1</div>
+          <h3 class="gd tc">老化予防</h3>
+              
+              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/medikaru/thumb02.jpg" alt="サムネイル"></div>
+              
+              <p class="gosic">進行する肌老化を抑制し、若々しいお肌を保つことができます</p>
+          
+          </li>
+          
+          <li class="box-size">
+              <div class="points f-ryu">Point.2</div>
+          <h3 class="gd tc">美肌効果</h3>
+              
+              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/medikaru/thumb03.jpg" alt="サムネイル"></div>
+          
+              <p class="gosic">たるみやシワ、シミといった肌トラブルを解消して、滑らかなお肌を保つことができます。</p>
+              
+          </li>
+          
+          <li class="box-size">
+              <div class="points f-ryu">Point.3</div>
+          <h3 class="gd tc">メスを入れない</h3>
+              
+              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/medikaru/thumb04.jpg" alt="サムネイル"></div>
+          
+              <p class="gosic">進行する肌老化を抑制し、若々しいお肌を保つことができます</p>
+              
+              
+          </li>
+          
+          <li class="box-size">
+              <div class="points f-ryu">Point.4</div>
+          <h3 class="gd tc pc">ダウンタイムがほとんどない</h3><h3 class="gd tc sp">ダウンタイムが<br>ほとんどない</h3>
+              
+              <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/operation/medikaru/thumb05.jpg" alt="サムネイル"></div>
+              
+              <p class="gosic">進行する肌老化を抑制し、若々しいお肌を保つことができます</p>
+          
+          </li>
+      </ul>
+      
+    
+  </div>
+</section>
+
+
+<section class="price">
+
+
+    <div class="inner-box2">
+    
+        
+        <h2 class="tc ls1">料金表</h2>
+        
+        
+        <div class="price-list gosic">
+        
+            <dl class="flex">
+                <dt>内容</dt>
+                <dd class="f-bold">税別</dd>
+            </dl>
+            
+             <dl class="flex">
+                <dt>内容入ります</dt>
+                <dd>¥00,000</dd>
+            </dl>
+            
+             <dl class="flex">
+                <dt>内容入ります</dt>
+                <dd>¥00,000</dd>
+            </dl>
+            
+            
+             <dl class="flex">
+                <dt>内容入ります</dt>
+                <dd>¥00,000</dd>
+            </dl>
+            
+             <dl class="flex">
+                <dt>内容入ります</dt>
+                <dd>¥00,000</dd>
+            </dl>
+        
+        </div>
+        
+        
+    
+    
+    </div>
+
+
+</section>
+<?php get_footer();?>
