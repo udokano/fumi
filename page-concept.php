48222e6a42571598fd9ccde80f69ef41bb964440
伊藤京子 <okano@u-d.jp>
2019-08-27 19:48:41 +0900

    アップ

--- page-concept.php
+++ page-concept.php
@@ -8,90 +8,64 @@ Template Name: 医院について
   <h1 class="tc f-ryu">当院について</h1>
   <?php echo breadcrumb_func(); ?> </div>
 <section class="vision pos mb0">
-  <div class="bg-under under1"></div>
   <div class="inner flex al-cent">
     <div class="left">
-      <h2><img src="<?php echo get_template_directory_uri(); ?>/img/concept/beautiful_aging.png" alt="Beautiful
+      <h2><img src="<?php echo get_template_directory_uri(); ?>/img/concept/beauitfulaging.png" alt="Beautiful
 Aging 私たちが目指すもの"></h2>
-        
-        <ul class="cachs flex">
-        
-            <li><img src="<?php echo get_template_directory_uri(); ?>/img/concept/sub_cach1.jpg" alt="安心・安全"></li>
-            <li><img src="<?php echo get_template_directory_uri(); ?>/img/concept/sub_cach2.jpg" alt="一人一人にあった治療"></li>
-            <li><img src="<?php echo get_template_directory_uri(); ?>/img/concept/sub_cach3.jpg" alt="誠実な治療"></li>
-
-        
-        </ul>
-        
-      <p>ご来院いただいた全ての皆様にマッチした治療を提供し、
+      <div class="img"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/points.png" alt=""></div>
+      <p class="tc">ご来院いただいた全ての皆様にマッチした治療を提供し、
         皆様がより自分らしく美しく年齢を重ね、
         豊かな人生を送れるようサポートをします。</p>
     </div>
-    <div class="right"> <img src="<?php echo get_template_directory_uri(); ?>/img/top/fv_logo.png" alt="Fumi Beauty clinic"> </div>
+    <div class="right"> <img src="<?php echo get_template_directory_uri(); ?>/img/concept/doctor_thumb.jpg" alt="先生"> </div>
   </div>
 </section>
-<section class="mission-area pos">
-  <div class="inner flex al-bottom mission1">
-    <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission01_thumb.jpg" alt="安心・安全な治療のみを
-提供いたします。">
-      <h3 class="rl ls3 gd sp">安心・安全な治療のみを<br>
-        提供いたします。</h3>
-    </div>
-    <div class="text left pos">
-      <div class="txt">
+<section class="mission-area ">
+  <div class="inner flex mission1">
+    <div class="box">
+      <div class="text ">
         <div class="img"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission01.png" alt="mission1"></div>
-        <p class="gosic mb30 ls1"> 日本の厚生労働省で承認を得ている製剤やFDAの承認を得ている安全性の確立された製剤を特におすすめしています。 </p>
-        <p class="gosic ls1"> また、なるべくダウンタイムやリスク、副作用のない治療をおすすめします。
-</p>
+        <h3 class="ls3 gd">安心・安全な治療のみを提供いたします。</h3>
+        <div class="txt">
+          <p class="gosic mb30 ls1"> 日本の厚生労働省で承認を得ている製剤やFDAの承認を得ている安全性の確立された製剤を特におすすめしています。 </p>
+          <p class="gosic ls1"> また、なるべくダウンタイムやリスク、副作用のない治療をおすすめします。</p>
+        </div>
       </div>
-      <h3 class="rl ls3 gd">安心・安全な治療のみを<br>
-        提供いたします。</h3>
     </div>
-  </div>
-  <!--MISSION1 END-->
-  
-  <div class="inner flex al-bottom mission2 ">
-    <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission02_thumb.jpg" alt="お一人お一人の悩みに寄り添い、
-確かな技術とセンスで、
-その方に合った自然な美しさを
-ご提案いたします。">
-      <h3 class="rl ls3 gd sp">お一人お一人の悩みに寄り添い、<br>
-        確かな技術とセンスで、<br>
-        その方に合った自然な美しさを<br>
-        ご提案いたします。</h3>
-    </div>
-    <div class="text left pos">
-      <div class="txt">
+      
+       <div class="box">
+      <div class="text ">
         <div class="img"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission02.png" alt="mission2"></div>
-        <p class="gosic mb30 ls1"> 私たちは丁寧なカウンセリングを心がけており、患者様のご要望やお悩みに寄り添い、その方に合った治療法をご提案いたします。 </p>
-        <p class="gosic ls1"> またナチュラルで違和感のない仕上がりを重視します。 </p>
+        <h3 class="ls3 gd">お一人お一人の悩みに
+寄り添い、確かな技術と
+センスでその方に合った
+自然な美しさをご提案
+いたします。</h3>
+        <div class="txt">
+          <p class="gosic mb30 ls1"> 私たちは丁寧なカウンセリングを心がけており、患者様のご要望やお悩みに寄り添い、その方に合った治療法をご提案いたします </p>
+          <p class="gosic ls1">またナチュラルで違和感のない仕上がりを重視します。</p>
+        </div>
       </div>
-      <h3 class="rl ls3 gd">お一人お一人の悩みに寄り添い、<br>
-        確かな技術とセンスで、<br>
-        その方に合った自然な美しさを<br>
-        ご提案いたします。</h3>
     </div>
-  </div>
-  <!--MISSION1 END-->
-  
-  <div class="inner flex al-bottom mission3">
-    <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission03_thumb.jpg" alt="クリーンで誠実な治療を
-お約束します。">
-      <h3 class="rl ls3 gd sp">クリーンで誠実な治療を<br>
-        お約束します。</h3>
-    </div>
-    <div class="text left pos">
-      <div class="txt">
+      
+       <div class="box mb0">
+      <div class="text ">
         <div class="img"><img src="<?php echo get_template_directory_uri(); ?>/img/concept/mission03.png" alt="mission3"></div>
-        <p class="gosic mb30 ls1"> 安心で安全な製剤を使用することはもちろん、分かりやすい料金設定で納得していただいた上で治療いたします。 </p>
-        <p class="gosic ls1"> また治療後の経過に関してもしっかりとしたフォローアップを行います。 </p>
+        <h3 class="ls3 gd">クリーンで誠実な
+治療をお約束します。</h3>
+        <div class="txt">
+          <p class="gosic mb30 ls1"> 安心で安全な製剤を使用することはもちろん、分かりやすい料金設定で納得していただいた上で治療いたします。</p>
+          <p class="gosic ls1">また治療後の経過に関してもしっかりとしたフォローアップを行います。</p>
+        </div>
       </div>
-      <h3 class="rl ls3 gd">クリーンで誠実な治療を<br>
-        お約束します。</h3>
     </div>
+      
   </div>
-  <!--MISSION1 END-->
+  <!--MISSION1 END--> 
   
-  <div class="bg-under bg-under2"></div>
+  <!--MISSION1 END--> 
+  
+  <!--MISSION1 END--> 
+  
 </section>
 <?php get_footer();?>
