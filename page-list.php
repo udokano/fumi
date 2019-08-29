ae0653a18db84e5f55ba034622b18a2791d1adde
伊藤京子 <okano@u-d.jp>
2019-08-29 19:41:00 +0900

    list_page_add

---
+++ page-list.php
@@ -0,0 +1,106 @@
+<?php
+/* 
+Template Name: 薬剤一覧 
+*/
+
+?>
+<?php get_header()?>
+<div class="page-head">
+  <h1 class="tc f-ryu">薬剤一覧</h1>
+  <?php echo breadcrumb_func(); ?> </div>
+<section class="ope-top-desc gosic">
+  <div class="ancor-box"> <a href="#area1">ヒアルロン酸</a><a href="#area2">ボトックス</a> </div>
+</section>
+<section class="list-intro bg-cp" id="area1">
+  <div class="inner">
+    <div class="text ls1">
+      <h2 class="tc">ヒアルロン酸</h2>
+      <p class="ls1 gosic">ほうれい線やマリオネットライン、ゴルゴライン(目の下のくぼみ)などはたるみになるしわといえます。
+        ヒアルロン酸でお顔全体をリフトアップさせたり、気になるしわやくぼみを埋めることで、自然な印象でたるみが改善し若々しい仕上がりとなります。</p>
+    </div>
+  </div>
+</section>
+<section class="drug-area">
+  <div class="drug-list">
+    <div class="inner">
+      <h3 class="tc ls1">ジュビダームビスタ® ボリューマ XC</h3>
+      <div class="box flex box-size">
+        <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/list/thumb01.png" alt="薬剤写真"></div>
+        <div class="text gosic">
+          <h4 class="cw ls1">効果持続期間目安：18～24ヶ月</h4>
+          <p class="ls1 mb35">アラガン・ジャパン社から発売されているヒアルロン酸でも硬いヒアルロン酸になります。主に、シワの改善、たるみの改善、顔のくぼみ(みぞ)の改善に使用されます。</p>
+          <p class="ls1">痛みに配慮し、快適に注入できるよう、ボリフト XCのヒアルロン酸の中に麻酔液が入っており、痛みに配慮した製品になっています。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+    <div class="drug-list">
+    <div class="inner">
+      <h3 class="tc ls1">ジュビダームビスタ® ボリフト XC</h3>
+      <div class="box flex box-size">
+        <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/list/thumb02.png" alt="薬剤写真"></div>
+        <div class="text gosic">
+          <h4 class="cw ls1">効果持続期間目安：12～18ヶ月</h4>
+          <p class="ls1 mb35">アラガン・ジャパン社から発売されているヒアルロン酸でも柔らかめのヒアルロン酸になります。主に、シワの改善、顔の溝、肌の質感の改善に使用されます。</p>
+          <p class="ls1">痛みに配慮し、快適に注入できるよう、ボリフト XCのヒアルロン酸の中に麻酔液が入っており、痛みに配慮した製品になっています。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+    <div class="drug-list">
+    <div class="inner">
+      <h3 class="tc ls1">ジュビダームビスタ® ボルベラ XC</h3>
+      <div class="box flex box-size">
+        <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/img/list/thumb03.png" alt="薬剤写真"></div>
+        <div class="text gosic">
+          <h4 class="cw ls1">効果持続期間目安：12ヶ月</h4>
+          <p class="ls1 mb35">アラガン・ジャパン社から発売されているヒアルロン酸でも柔らかめのヒアルロン酸になります。主に、シワの改善、顔の溝、肌の質感の改善に使用されます。</p>
+          <p class="ls1">痛みに配慮し、快適に注入できるよう、ボルベラ XCのヒアルロン酸の中に麻酔液が入っており、痛みに配慮した製品になっています。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+     <div class="drug-list border-none">
+    <div class="inner">
+      <h3 class="tc ls1">クレヴィエルコントア</h3>
+      <div class="box flex box-size">
+        <div class="thumb cvr"><img src="<?php echo get_template_directory_uri(); ?>/img/list/thumb04.png" alt="薬剤写真"></div>
+        <div class="text gosic">
+          <h4 class="cw ls1">効果持続期間目安：12～15ヶ月</h4>
+          <p class="ls1 mb35">クレヴィエルは、Amore Pacific社から発売されているヒアルロン酸です。当院でお取り扱いしているコントアは高濃度・高密度ヒアルロン酸で、形が崩れにくく、ヒアルロン酸の中では長持ちする特徴があり、お顔の印象をシャープにされたい方に適したヒアルロン酸です。</p>
+          <p class="ls1">こちらも麻酔液が入っており、痛みに配慮した製品になっています。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="list-intro bg-cp" id="area2">
+  <div class="inner">
+    <div class="text ls1">
+      <h2 class="tc">ボトックス</h2>
+      <p class="ls1 gosic">お顔は表情を作るために様々な筋肉で構成され、毎日のお顔の動きによって折り紙の折ジワのように年齢とともに表情じわが刻まれてしまいます。
+目尻の笑いじわ、眉間や鼻のしわ、額の横じわ、あごの梅干しじわなどがボトックスの適応となります。
+ボトックスを打つことで表情が柔らかくなりお顔の印象も優しくなるのも利点です。</p>
+    </div>
+  </div>
+</section>
+<section class="drug-area">
+  <div class="drug-list border-none">
+    <div class="inner">
+      <h3 class="tc ls1">ボトックスビスタ®</h3>
+      <div class="box flex box-size">
+        <div class="thumb btx"><img src="<?php echo get_template_directory_uri(); ?>/img/list/thumb05.png" alt="薬剤写真"></div>
+        <div class="text gosic">
+          <h4 class="cw ls1">効果持続期間目安：3～6ヶ月</h4>
+          <p class="ls1 mb35">アラガン・ジャパン社から発売されているボトックスです。
+ボトックスビスタ®は、ボツリヌス菌Clostridium botulinumにより産生されるA型ボツリヌス毒素を有効成分とする筋弛緩剤です。</p>
+          <p class="ls1">作用機序としては「表情じわ」を形成する表情筋に注射することによって、筋肉の収縮を抑制し、一定期間「表情じわ」を改善します。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer();?>
