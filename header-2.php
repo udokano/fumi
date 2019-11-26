dc4b506279e0911db0871095da773276bcc527fd
伊藤京子 <okano@u-d.jp>
2019-11-26 15:49:52 +0900

    パラメータ更新

--- header-2.php
+++ header-2.php
@@ -7,7 +7,7 @@
 <meta name="format-detection" content="telephone=no">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/css/swiper.min.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css?1118">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css?1226">
     <script>
   (function(d) {
     var config = {
@@ -37,7 +37,6 @@
     <div class="header-inner flex al-cent">
       <div class="logo tc"><a href="<?php echo home_url('/'); ?>">
           <img src="<?php echo get_template_directory_uri(); ?>/img/common/logo.png" alt="FMUI BEAUTY CLINIC"></a></div>
-
       <div class="top-cv-btn"> <a class="bg-rs cv-btns" href="https://fumibeauty.reserve.ne.jp/" target="_blank"><span>来院予約</span></a> <a class="bg-ct cv-btns" href="<?php echo home_url('/'); ?>contact-list"><span>お問合わせ</span></a> </div>
     </div>
   </header>
